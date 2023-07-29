63f21810df22e4980ae77f7382fbed0baad24788
len5464 <zawaleng@gmail.com>
2023-07-29 17:38:43 +0800

    修改

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: "/vite-test/",
+  base: "/2023web-layout-training_glasses-website/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
